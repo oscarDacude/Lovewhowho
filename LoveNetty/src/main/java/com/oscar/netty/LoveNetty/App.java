f99c0c77a0938bb84b79a269bf92389cbe55b39d
OscarDacude <2256828336@qq.com>
2019-07-02 15:49:53 +0800

    新创建一个项目，添加所有文件

---
+++ LoveNetty/src/main/java/com/oscar/netty/LoveNetty/App.java
@@ -0,0 +1,13 @@
+package com.oscar.netty.LoveNetty;
+
+/**
+ * Hello world! at 20190702
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Netty! I'm coming!" );
+    }
+}
